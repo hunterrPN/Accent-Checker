e930cc89d81a401bc94a64fc93cfb84c946922a9
Pratik Nainwal <nainwalpratik780@gmail.com>
2025-07-18 17:17:07 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Pratik Nainwal",
+    author_email="23143@iiitu.ac.in",
+    packages=find_packages()
+)
